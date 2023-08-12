51c9145ff640bf82547f4ae468ff82d292bc4c6f
Bogdan4D <bdobre44@hmail.com>
2023-08-12 17:32:25 +0300

    styled loading component

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,7 +30,7 @@ class App extends Component  {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
 		})
 		return !robots.length ?
-		<h1>Loading</h1> :
+		<h1 className='loading'>Loading...</h1> :
 		(
 			<div className='tc'>
 				<h1 className='f2'>RoboFriends</h1>
